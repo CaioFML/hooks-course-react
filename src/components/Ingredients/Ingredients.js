254b0397d264c2968de8bfc71a0efa2ff991dbe6
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-01-17 16:38:48 -0300

    432. Understanding useEffect() Dependencies

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -26,6 +26,10 @@ const Ingredients = () => {
       });
   }, []);
 
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients)
+  }, [userIngredients])
+
   const addIngredientHandler = (ingredient) => {
     fetch(
       "https://hooks-react-test-fa578-default-rtdb.firebaseio.com/ingredients.json",
