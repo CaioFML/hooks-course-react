2052c850ed0cd1a6a564e7c0653ec9716666d7b2
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-01-17 17:03:23 -0300

    433. More on useEffect()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -30,6 +30,10 @@ const Ingredients = () => {
     console.log('RENDERING INGREDIENTS', userIngredients)
   }, [userIngredients])
 
+  const filteredIngredientsHandler = filteredIngredients => {
+    setUserIngredients(filteredIngredients)
+  }
+
   const addIngredientHandler = (ingredient) => {
     fetch(
       "https://hooks-react-test-fa578-default-rtdb.firebaseio.com/ingredients.json",
@@ -61,7 +65,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList
           ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler}
