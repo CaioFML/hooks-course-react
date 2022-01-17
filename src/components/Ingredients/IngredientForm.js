9359b46330a4bb83b933ee7ff65764d371ea3f6b
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-01-17 14:01:34 -0300

    429. Passing State Data Across Components

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -10,7 +10,7 @@ const IngredientForm = React.memo((props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
