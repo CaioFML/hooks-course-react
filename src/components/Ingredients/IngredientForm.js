daffa261cb5fbce4d48a44b9ad006fc774b141d9
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-01-17 13:00:51 -0300

    424. Getting Started with useState()

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const [inputState, setInputState] = useState({ title: "", amount: "" });
+
+  const submitHandler = (event) => {
     event.preventDefault();
     // ...
   };
@@ -15,11 +17,23 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={inputState.title}
+              onChange={(event) => setInputState({ title: event.target.value })}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={inputState.amount}
+              onChange={(event) =>
+                setInputState({ title: event.target.amount })
+              }
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
